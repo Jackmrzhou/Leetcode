7107b46c09fc67336280e0527a6080b525e6d7ca
yves <yveszhou.zyc@gmail.com>
2021-08-23 21:27:30 +0800

    2021/08/23 Mon

---
+++ scala/535.scala
@@ -0,0 +1,11 @@
+class Codec {
+    // Encodes a URL to a shortened URL.
+    def encode(longURL: String): String = {
+        longURL
+    }
+    
+    // Decodes a shortened URL to its original URL.
+    def decode(shortURL: String): String = {
+        shortURL
+    }
+}
